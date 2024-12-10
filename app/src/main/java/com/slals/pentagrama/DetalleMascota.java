ea12462a7d83636f6bc56f318be86f41a988153f
slalozanos <sl.lozanos@lanuevaamerica.edu.co>
2024-12-09 20:16:26 -0500

    Initial commit

---
+++ app/src/main/java/com/slals/pentagrama/DetalleMascota.java
@@ -0,0 +1,13 @@
+package com.slals.pentagrama;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetalleMascota extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mascota_detalle);
+    }
+}
